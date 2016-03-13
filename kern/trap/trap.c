ac4ad946705f003366ebe953979b80bc61927218
Leonard Xu <leopard.lie@gmail.com>
2016-03-13 11:31:19 +0800

    enable sys call

--- kern/trap/trap.c
+++ kern/trap/trap.c
@@ -61,7 +61,7 @@ idt_init(void) {
     for (i = 0; i < 256; i++) {
         SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
     }
-    SETGATE(idt[T_SWITCH_TOK], 0, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
+    SETGATE(idt[T_SYSCALL], 0, GD_KTEXT, __vectors[T_SYSCALL], DPL_USER);
     lidt(&idt_pd);
 }
 
@@ -235,6 +235,7 @@ trap_dispatch(struct trapframe *tf) {
         if (ticks == 100) {
             ticks = 0;
             print_ticks();
+            current->need_resched = 1;
         }
         break;
     case IRQ_OFFSET + IRQ_COM1:
